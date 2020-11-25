f24fb653663a2f479a30911dfbdf7676bf888e07
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-11-25 23:42:17 +0530

    Update iterator.py

--- iterator.py
+++ iterator.py
@@ -2,3 +2,21 @@ iterator = iter('abc')
 print(next(iterator))
 print(next(iterator))
 print(next(iterator))
+
+# using object oriented programming
+class Iterator(object):
+    def __init__(self, step):
+        self.step = step
+
+    def __next__(self):
+        if self.step == 0:
+            raise StopIteration
+        self.step -= 1
+        return self.step
+
+    def __iter__(self):
+        return self
+
+if __name__ == "__main__":
+    itera = Iterator(5)
+    [print(value) for value in itera]
